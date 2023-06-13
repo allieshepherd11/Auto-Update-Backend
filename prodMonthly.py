ee26364cfcb87ccc9cdb1c2a36b2950ea4b04aaf
DanielMPang <114116375+DanielMPang@users.noreply.github.com>
2023-06-13 13:44:41 -0500

    created cumulative data json + csv

--- prodMonthly.py
+++ prodMonthly.py
@@ -6,12 +6,13 @@ import requests
 from collections import OrderedDict
 import json
 
-# Run this the start of every month to update monthly production from the last month
+# RUN THIS THE START OF EVERY MONTH TO UPDATE MONTHLY PRODUCTION TO INCLUDE THE PREVIOUS MONTH
 def main():
-    createMdataByNameJson()
+    updateMonthlyData()
+    updateCumMoData()
 
 
-def createMdataByNameJson():
+def updateMonthlyData():
     df_daily = pd.read_csv('data.csv')
     df_daily['Date'] = pd.to_datetime(df_daily['Date'])
     df_daily['Month'] = df_daily['Date'].dt.month
@@ -24,24 +25,37 @@ def createMdataByNameJson():
     monthly_sum = monthly_sum.drop('Year', axis=1)
     monthly_sum = monthly_sum.drop('Month', axis=1)
 
-    monthly_sum.to_csv("monthlySumData.csv", index=False)
+    monthly_sum.to_csv("monthlyDataST.csv", index=False)
 
-    # FILE DESTINATION, CHANGE TO FIT YOUR LOCAL GITHUB FOLDER. File name: "sumDataMonthly.json"
-    monthly_sum.to_json("../prod-1/data/sumDataMonthly.json", orient='values', date_format='iso')
+    # FILE DESTINATION, CHANGE TO FIT YOUR LOCAL GITHUB FOLDER. File name: "dataMonthlyST.json"
+    monthly_sum.to_json("../prod-1/data/dataMonthlyST.json", orient='values', date_format='iso')
     #------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^-----------------------------------------#
 
-    exit()
 
-main()
-
-# Unused
-def createMdataCSV():
+def updateCumMoData():
     df_daily = pd.read_csv('data.csv')
     df_daily['Date'] = pd.to_datetime(df_daily['Date'])
     df_daily['Month'] = df_daily['Date'].dt.month
     df_daily['Year'] = df_daily['Date'].dt.year
 
-    grouped = df_daily.groupby(['Year', 'Month'])
-    monthly_sum = grouped.sum()
+    grouped = df_daily.groupby(['Well Name', 'Year', 'Month'])
+    monthly_sum = grouped.sum().reset_index()
 
-    monthly_sum.to_csv('Mdata.csv')
+    # Calculate the cumulative sum on selected columns
+    group = monthly_sum.groupby('Well Name')
+    final_cumsum = group['Oil (BBLS)', 'Gas (MCF)', 'Water (BBLS)'].cumsum()
+    monthly_sum[['Oil (BBLS)', 'Gas (MCF)', 'Water (BBLS)']] = final_cumsum
+
+    # Date column, drop year & month columns
+    monthly_sum['Date'] = pd.to_datetime(monthly_sum[['Year', 'Month']].assign(day=1))
+    monthly_sum = monthly_sum.drop('Year', axis=1)
+    monthly_sum = monthly_sum.drop('Month', axis=1)
+
+    monthly_sum.to_csv("monthlyCumDataST.csv", index=False)
+
+    # FILE DESTINATION, CHANGE TO FIT YOUR LOCAL GITHUB FOLDER. File name: "cumDataMonthlyST.json"
+    monthly_sum.to_json("../prod-1/data/cumDataMonthlyST.json", orient='values', date_format='iso')
+    #------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^-----------------------------------------#
+
+
+main()
