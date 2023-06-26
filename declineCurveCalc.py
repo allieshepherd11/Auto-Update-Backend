7f5462e6e408b7f0579a15d45fea9111e7df7c57
DanielMPang <114116375+DanielMPang@users.noreply.github.com>
2023-06-26 17:59:09 -0500

    in process of implementing a manual df for starting dates

--- declineCurveCalc.py
+++ declineCurveCalc.py
@@ -8,28 +8,44 @@ from dateutil.relativedelta import relativedelta
 
 # RUN prodMonthly.py BEFORE THIS TO HAVE UPDATED DECLINE CURVES
 def main():
+    
+    manualEntry()
+
+    exit()
+    
     df_wells = pd.read_json('everyWell.json')
     my_list = df_wells.values.tolist()
+    print(my_list)
 
     oneDList = [element for sublist in my_list for element in sublist]
     params = {}
-    for i in oneDList:
-        well_params = declineCurve(i) 
-        if well_params != None:
-            params[i] = well_params
+    for well in oneDList:
+        well_params = declineCurve(well)
+        well = well.replace("#", "").replace(" ", "").lower()
+        # if well_params != None:
+        params[well] = well_params
 
     params = pd.DataFrame(params)
     # FILE DESTINATION, CHANGE TO FIT YOUR LOCAL GITHUB FOLDER. File name: "dataMonthlyST.json"
-    # params.to_csv("../prod/data/declineCurves/1params.csv", index=False)
+    params.to_csv("../prod/data/declineCurves/1params.csv", index=False)
     #----------------^^^^^--------------------------------------------------------------------#
+
+def manualEntry():
+    dfManual = pd.read_csv('dManualEntries.csv')
+    name = input("Enter well name: ")
+    startMonth = input("Enter starting month: ")
+
+    dfNew = {'Well Name': name, 'Start': startMonth}
+    dfManual = pd.concat(dfManual, dfNew)
+    dfManual.to_csv("dManualEntries.csv", index=False)
     
 def declineCurve(name):
-    data = pd.read_csv('monthlyDataST.csv')
+    data = pd.read_csv('db/prod/monthlyDataST.csv')
     # Drop unused columns, exclude data from the current month
     df = data[data['Well Name'] == name].drop(columns=['Gas (MCF)', 'Water (BBLS)', 'TP', 'CP'])
     df_format = df.drop(df.index[-1])
     df_format.to_csv("dectest.csv", index=False)
-    df_data = pd.read_csv('dectest.csv')
+    df_data = pd.read_csv('dectest.csv') # This csv needs to be kept to run
 
     # CREATE REAL DATA ARRAYS TO BE USED ON FINAL GRAPH
     # t_real = np.arange(len(df_data))
@@ -39,48 +55,53 @@ def declineCurve(name):
     exclList = []
 
     try:
-        # FOR LOOP SUMMARY
-        """
-        # Check the next 5 months and make sure they are not increasing, if not move on to next month
-        # Check the next 10 months from the month ^ loop ended on
-            # Check 1stmonth - 2ndmonth, if > highest_decrease, assign 2nd month as highest decrease
-            # Check 2nd mont - 3rd month, if > highest_decrease, assign 3rd month as highest decrease
-            # ...
-            # Check 9th month - 10th month, if > highest_decrease, assign 10th month as highest decrease
-        # Choose the one with the highest decrease (the month after the decrease happens)
-        # Delete everything before said month
-        """
-        drop_index = None # Index such that everything before will be dropped
-        mtc = 5 # Months_To_Check
-        r = 10 # Range of Months to check for the highest decreasing
+        if(1==1):
+        # if well.isin(dfManual):
+            # drop_index = dfManual.loc[dfManual['Well Name']==name, 'Start'].values[0]
+        # else:
+            drop_index = None # Index such that everything before will be dropped
+            mtc = 5 # Months_To_Check
+            r = 10 # Range of Months to check for the highest decreasing
 
-        # Loop through indexes in df_data
-        for i in range(len(df_data)-mtc):
-            tally = 0 # Set counter to 0
+            # FOR LOOP SUMMARY
+            """
+            # Check the next 5 months and make sure they are not increasing, if not move on to next month
+            # Check the next 10 months from the month ^ loop ended on
+                # Check 1stmonth - 2ndmonth, if > highest_decrease, assign 2nd month as highest decrease
+                # Check 2nd mont - 3rd month, if > highest_decrease, assign 3rd month as highest decrease
+                # ...
+                # Check 9th month - 10th month, if > highest_decrease, assign 10th month as highest decrease
+            # Choose the one with the highest decrease (the month after the decrease happens)
+            # Delete everything before said month
+            """
+            # Loop through indexes in df_data
+            for i in range(len(df_data)-mtc):
+                tally = 0 # Set counter to 0
 
-            # Check the next 4 months and make sure they are not increasing
-            for index, row in df_data.iloc[i:i+mtc].iterrows(): 
-                if index==len(df_data)-1: # Break if index is at the very end of the loop (1 case)
+                # Check the next 4 months and make sure they are not increasing
+                for index, row in df_data.iloc[i:i+mtc].iterrows(): 
+                    if index==len(df_data)-1: # Break if index is at the very end of the loop (1 case)
+                        break
+                    if row["Oil (BBLS)"] < df_data.loc[index+1, "Oil (BBLS)"]: # If one of the values being checked is increasing (relative to value after), add a tally
+                        tally+=1
+                
+                if tally == 0: # If no values were found to be increasing, check the next 10 months for the one with the highest decrease
+                    highest_decrease = 0
+                    index = i
+
+                    # Loop starting at 0 until reaches range r
+                    for loop in range(r):
+                        if df_data.loc[i+loop, "Oil (BBLS)"] - df_data.loc[i+loop+1, "Oil (BBLS)"] > highest_decrease: # Check if row has the highest decrease
+                            highest_decrease = df_data.loc[i+loop, "Oil (BBLS)"] - df_data.loc[i+loop+1, "Oil (BBLS)"] # save highest decrease
+                            index = i + loop + 1 # save index of highest decrease (the row after the current one)
+                    # Assign drop_index to be the index after the for loop
+                    drop_index = index
                     break
-                if row["Oil (BBLS)"] < df_data.loc[index+1, "Oil (BBLS)"]: # If one of the values being checked is increasing (relative to value after), add a tally
-                    tally+=1
-            
-            if tally == 0: # If no values were found to be increasing, check the next 10 months for the one with the highest decrease
-                highest_decrease = 0
-                index = i
-
-                # Loop starting at 0 until reaches range r
-                for loop in range(r):
-                    if df_data.loc[i+loop, "Oil (BBLS)"] - df_data.loc[i+loop+1, "Oil (BBLS)"] > highest_decrease: # Check if row has the highest decrease
-                        highest_decrease = df_data.loc[i+loop, "Oil (BBLS)"] - df_data.loc[i+loop+1, "Oil (BBLS)"] # save highest decrease
-                        index = i + loop + 1 # save index of highest decrease (the row after the current one)
-                # Assign drop_index to be the index after the for loop
-                drop_index = index
-                break
-
+        
+        exit()
         actual_df = df_data.drop(df_data.index[0:drop_index])
         dates_model = actual_df['Date'].values.tolist()
-        actual_df.to_csv("dekAnswer.csv", index=False)
+        # actual_df.to_csv("dekAnswer.csv", index=False)
 
         # Create time and production lists
         t = np.arange(len(actual_df))
@@ -127,11 +148,11 @@ def declineCurve(name):
         dict_final = {'t': t_real, 'q': q_real, 't_model': dates_model, 'q_model': q_model}
         df_final = pd.DataFrame(dict_final)
 
-        name = name.replace("#", "").replace(" ", "")
+        name = name.replace("#", "").replace(" ", "").lower()
         # FILE DESTINATION, CHANGE TO FIT YOUR LOCAL GITHUB FOLDER. File name: "dataMonthlyST.json"
-        # df_final.to_csv(f"../prod/data/declineCurves/{name}.csv", index=False)
+        df_final.to_csv(f"../prod/data/declineCurves/{name}.csv", index=False)
         #------------------^^^^^^^----------------------------------------------------------------#
-        return {"qi":qi_est, "di":Di_est, "b":b_est, "extr_mo":extr_mo, "q_sum": q_eco, "qm_sum": qm_eco, "future_prod":fp_eco, "eco_limit_mo": moUntilEcoLimit}
+        return {"qi":qi_est, "D":Di_est, "b":b_est, "extr_mo":extr_mo, "q_sum": q_eco, "qm_sum": qm_eco, "future_prod":fp_eco, "eco_limit_mo": moUntilEcoLimit}
     
     # except RuntimeWarning:
     #     print("Error: Invalid conversion to integer.")
@@ -150,23 +171,33 @@ def hyperbolicEq(t, qi, b, Di):
 def economics(q, q_model, time, qi_est, b_est, Di_est, extr_mo):
     q_sum = sum(q)
     q_model_sum = sum(q_model)
-    future_prod = q_model_sum - q_sum
+    future_prod = q_model_sum - q_sum #This is not entirely correct, need to subtract from q_model before extrapolate UPDATE LATER
 
-    extra_months = list(range(len(time), len(time) + extr_mo))  # Generate a range of values based on the length of t
+    extra_months = list(range(max(time), max(time) + extr_mo))  # Generate a range of values based on the length of t
     time = time.tolist()
     time += extra_months  # Concatenate the original list with the extra months)
 
-    moUntilEcoLimit = None
-    for i in time:
+    moUntilEcoLimit = 'infinity'
+    for i in time:  
         if qi_est/(1+b_est*Di_est*i)**(1/b_est) <= 140:
             moUntilEcoLimit = i
             break
+    
+    # gasPrice = 70
+    # roy_tax = .70875
+    # operCost = 10000
+
+    # netValue = (oil*70*.70875*30) - 10000
+    
+    # discount_rate = 0.1
+    # cash_flows = [-1000, 500, 300, 200, 100]
+
+    # npv = sum(cf / (1 + discount_rate) ** n for n, cf in enumerate(cash_flows))
 
     return q_sum, q_model_sum, future_prod, moUntilEcoLimit
 
-    
-    
 main()
+
 
 # OLD CODE =========================================================================================
 
